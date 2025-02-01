54738ffd4d826df22b872d8d6b1b87a377a6d66d
shivachaudhary46 <shivachaudhary4116@gmail.com>
2025-02-01 10:10:21 +0545

    binary trees and patterns

--- patterns/pattern_8_pyramid.cpp
+++ patterns/pattern_8_pyramid.cpp
@@ -4,9 +4,9 @@ using namespace std;
 int main() {
 	int row = 1, tot = 5;
 	while(row <= tot) {
-		col = 1;
+		int col = 1;
 		while(col <= row) {
-			k = 1;
+			int k = 1;
 			while(k <= row) {
 				k = tot - k + 1;
 			
