03412b5bcb4118163e54a76c125ce1794c6e73ed
shivachaudhary46 <shivachaudhary4116@gmail.com>
2025-02-08 17:39:41 +0545

    modified for wave priint and corrected

--- array/wave_print copy.cpp
+++ array/wave_print copy.cpp
@@ -4,7 +4,7 @@ vector<int> wavePrint(int nums[][4], int nrows, int ncols){
     vector<int> ans;
     for(int col=0; col<=ncols; col++){
         if(col&1){//odd number from bottom to top@ 
-            for(int row=nrows-1; row>=0; row==){
+            for(int row=nrows-1; row>=0; row--){
                 ans.push_back(nums[row][col]);
             }
 
--- array/wave_print.cpp
+++ array/wave_print.cpp
@@ -1,27 +1,44 @@
-#include<iostream>
+#include <iostream>
+#include <vector>
 using namespace std;
-vector<int> wavePrint(int nums[][4], int nrows, int ncols){
-    vector<int> ans;
-    for(int col=0; col<=ncols; col++){
-        if(col&1){//odd number from bottom to top@ 
-            for(int row=nrows-1; row>=0; row==){
-                ans.push_back(nums[row][col]);
-            }
 
-        }else{
-            for(int row=0; row<nrows; row++){
+vector<int> wavePrint(int nums[][6], int nrows, int ncols) {
+    vector<int> ans;
+
+    for (int col = 0; col < ncols; col++) {
+        if (col & 1) { // Odd column: bottom to top
+            for (int row = nrows - 1; row >= 0; row--) {
+                ans.push_back(nums[row][col]);
+            }
+        } else { // Even column: top to bottom
+            for (int row = 0; row < nrows; row++) {
                 ans.push_back(nums[row][col]);
             }
         }
     }
+
+    return ans;
 }
-int main(){
+
+int main() {
     int nums[4][6];
-    for(int i=0; i<4; i++){
-        for(int j=0; j<6;  j++){
-            cout<<"enter* "<<i<<j<<" ";
-            cin>>nums[i][j];
+
+    // Taking input
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 6; j++) {
+            cout << "Enter element [" << i << "][" << j << "]: ";
+            cin >> nums[i][j];
         }
     }
-    
+
+    // Calling wavePrint function
+    vector<int> result = wavePrint(nums, 4, 6);
+
+    // Printing result
+    cout << "Wave Print Output: ";
+    for (int num : result) {
+        cout << num << " ";
+    }
+
+    return 0;
 }
