3e40886a0806f8ecd6084617496470bc31705809
shivachaudhary46 <shivachaudhary4116@gmail.com>
2026-02-05 05:03:42 +0545

    book allocation method:

---
+++ array/book_allocation.py
@@ -0,0 +1,49 @@
+def isPossible(arr, n, m, mid): 
+    pageSum = 0 
+    studentCount = 1 
+
+    for i in range(len(arr)): 
+        if (pageSum + arr[i] <= mid): 
+            pageSum += arr[i]
+
+        else:
+            studentCount = studentCount + 1 
+            if studentCount < m | arr[i] > mid : 
+                return -1 
+            pageSum = arr[i]
+
+# n is number of the student
+# m is minimum number of books assigned to each students 
+
+# Each student receives atleast one book 
+# Each student is assigned a contiguous sequence of books 
+# No book is assigned to more than one student
+def bookAllocation(arr, n, m): 
+
+    s = 0 
+    
+    total = 0
+    for i in range(len(arr)): 
+        total += arr[i]
+
+    e = total 
+
+    mid = s + (e-s) / 2 
+    ans = 0
+
+    while (s <= e):
+        if isPossible(arr, n, m, mid): 
+            ans = mid 
+            e = mid 
+        else: 
+            s = mid + 1 
+
+        mid = s + (e-s) / 2 
+    
+    return ans 
+
+if __name__ == "__main__":
+    arr = [10, 20, 30, 40]
+    ans=bookAllocation(arr, len(arr), 2)
+    print(ans)
+    
