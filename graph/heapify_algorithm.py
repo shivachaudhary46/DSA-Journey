86d39cd4aabbbf64426ccf52a60b0f41ae6fd3e3
shivachaudhary46 <shivachaudhary4116@gmail.com>
2026-02-08 18:06:59 +0545

    added ma x heap into the heapify algorithm

--- graph/heapify_algorithm.py
+++ graph/heapify_algorithm.py
@@ -24,6 +24,67 @@ class heapify:
         
         print()
 
+    def deleteheap(self): 
+        self.arr[1] = self.arr[self.size]
+        self.size = self.size - 1 
+
+        index = 1 
+        while (index < self.size):
+            largest = index
+            leftIndex = 2 * index
+            rightIndex = 2 * index + 1
+
+            if leftIndex < self.size and self.arr[leftIndex] > self.arr[index]: 
+                largest = leftIndex
+
+            elif rightIndex < self.size and self.arr[rightIndex] > self.arr[index]: 
+                largest = rightIndex
+            else: 
+                return
+            
+            if largest != index: 
+                self.arr[largest], self.arr[index] = self.arr[index], self.arr[largest]
+
+    def deleteHeap1(self): 
+
+        self.arr[1] = self.arr[self.size]
+        self.size = self.size - 1 
+
+        index = 1
+        while(index < self.size): 
+            leftIndex = 2 * index 
+            rightIndex = 2 * index + 1
+
+            if leftIndex < self.size and self.arr[leftIndex] > self.arr[index]: 
+                self.arr[leftIndex], self.arr[index] = self.arr[index], self.arr[leftIndex]
+                index = leftIndex
+            
+            elif rightIndex < self.size and self.arr[rightIndex] > self.arr[index]: 
+                self.arr[rightIndex], self.arr[index] = self.arr[index], self.arr[rightIndex]
+                index = rightIndex
+
+            else: 
+                return
+            
+    def max_heapify(self, arr, n, i): 
+        largest = i 
+        left = 2 * i 
+        right = 2 * i + 1
+
+        while ( i < n): 
+            if left < n and arr[left] < arr[i]: 
+                largest = left
+            elif right < n and arr[right] < arr[i]: 
+                largest = right 
+            else: 
+                return 
+
+             
+    def max_heapify(self, arr, ):
+        pass
+        # clear
+        
+
 if __name__ == "__main__": 
     h = heapify() 
     h.insert(78)
@@ -35,5 +96,10 @@ if __name__ == "__main__":
 
     h.show() 
 
-            
-        
+    # h.deleteheap()
+    
+    # h.show()
+
+    h.deleteHeap1()
+    
+    h.show() 
